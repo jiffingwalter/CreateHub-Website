d6a9af982d2cebc4eb70866ade484c8281a85a6e
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-01-20 18:23:28 -0500

    Revert "Destroyed EVERYTHING!"
    This reverts commit 689c596df7101d43bbf747736735ff8b2b0d6ac9.
    
    Reverted Angular lol

--- src/app/post.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-    id: number;
-    title: string;
-    author: string;
-    description: string;
-    media: boolean;
-    dateCreated: Date;
-    dateModified: Date;
-}
--- src/app/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: number;
-    username: string;
-    email: string;
-    password: string;
-    dateCreated: Date;
-    dateLastSignedin: Date;
-    role: number;
-}
